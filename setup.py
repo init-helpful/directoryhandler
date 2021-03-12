72828e136a5ea05be2fb95af109e18e5c9aa9653
cummingsa <57905451+cummingsa@users.noreply.github.com>
2021-03-11 21:12:28 -0500

    Updated for packaging

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="directoryhandler",
+    version="0.1.0",
+    description="Local directory lookup and management",
+    install_requires=["jsondatahelper"],
+    packages=find_packages(),
+)
